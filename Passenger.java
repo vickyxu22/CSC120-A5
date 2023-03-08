69e3a3e1f2e331339ace09410af1d27ce3e2f85e
vickyxu22 <113194676+vickyxu22@users.noreply.github.com>
2023-03-08 13:38:20 -0500

    Update Passenger.java

--- Passenger.java
+++ Passenger.java
@@ -1,8 +1,43 @@
 public class Passenger {
-    
+
     private String name;
 
     public Passenger(String name) {
         this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void boardCar(Car car) {
+        try {
+            car.addPassenger(this);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void getOffCar(Car car) {
+        try {
+            car.removePassenger(this);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public String toString() {
+        return name;
+    }
+
+    public static void main(String[] args) {
+        Passenger passenger = new Passenger("Vicky");
+        try {
+            while (true) {
+                passenger.toString();
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage()); // Out of fuel
+        }
     }
 }
